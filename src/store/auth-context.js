2af71dd79ad37daaffbf0ee73de207ee8b7d1824
Samair <samair.zia@tenpearls.com>
2023-02-21 16:53:10 +0500

    useContext hook for consuming context

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 //Context is just app or component wide state, it can be anything, string, object etc
-const AuthContext =  React.createContext({
-  isLoggedIn: false
-})
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+});
 
 export default AuthContext;
